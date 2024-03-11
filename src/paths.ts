66655b8a65c2304cbd39fe81587e9dd7a2512ce3
tonyishangu <tonyishangu@gmail.com>
2024-03-11 21:57:38 +0300

    implement a path finder

---
+++ src/paths.ts
@@ -0,0 +1,16 @@
+const Paths = {
+    home () {
+        return '/'
+    },
+    showTopic (topicSlug: string) {
+        return `/topics/${topicSlug}/`
+    },
+    createPost(topicSlug: string) {
+        return `/topics/${topicSlug}/posts/new/`
+    },
+    showPost(topicSlug: string, postId: string) {
+        return `/topics/${topicSlug}/posts/${postId}`
+    }
+}
+
+export default Paths
