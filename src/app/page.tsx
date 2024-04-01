2dd178aef274b3678814fa399440d0af12fa78b3
tonyishangu <tonyishangu@gmail.com>
2024-04-01 12:31:29 +0300

    static caching while using auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/app/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/Profile";
-
-
-export default async function Home() {
-
-  const session = await auth()
-
+export default function Home() {
 
   return (
-    <>
-      <form action={actions.signIn}>
-        <Button type="submit">Login</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Logout</Button>
-      </form>
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> :
-        <div>Signed Out</div>
-      }
-      <Profile />
-    </>
+    <div>
+      Home Page
+    </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,55 +5,12 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
 } from "@nextui-org/react";
 // check authentication
-import { auth } from "@/auth";
-import * as actions from "../app/actions";
+import HeaderAuth from "./headerAuth";
 
-export default async function Header() {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement="left">
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ""} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className="p-4">
-            <form action={actions.signOut}>
-              <Button type="submit">Sign out</Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Login
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Register
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
+export default function Header() {
+  
 
   return (
     <Navbar className="shadow mb-6">
@@ -67,7 +24,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authContent}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
