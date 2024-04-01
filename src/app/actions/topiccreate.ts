129e1f675803cfa5aeb48af17a0bf4861f42a294
tonyishangu <tonyishangu@gmail.com>
2024-04-01 14:27:23 +0300

    form validation using Zod

--- src/app/actions/topiccreate.ts
+++ src/app/actions/topiccreate.ts
@@ -1,9 +1,26 @@
-"use server"
+"use server";
+
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
 export async function createTopic(formData: FormData) {
-    // TODO:REVALIDATE THE HOMEPAGE
-    const name = formData.get('name')
-    const description = formData.get('description')
+  const result = createTopicSchema.safeParse({ 
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
 
-    console.log(name, description)
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
+  // TODO:REVALIDATE THE HOMEPAGE
 }
