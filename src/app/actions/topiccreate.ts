239513b04094fb67eab524fe7b3de3a43c65d80f
tonyishangu <tonyishangu@gmail.com>
2024-04-07 12:17:14 +0300

    handling form validation and errors

--- src/app/actions/topiccreate.ts
+++ src/app/actions/topiccreate.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import { auth } from "@/auth";
 
 const createTopicSchema = z.object({
   name: z
@@ -12,15 +13,41 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
-  const result = createTopicSchema.safeParse({ 
+interface CreateTopicFormSate {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[]
+  };
+}
+
+export async function createTopic(
+  formSate: CreateTopicFormSate,
+  formData: FormData
+): Promise <CreateTopicFormSate> {
+  const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors
+    }
   }
 
+  const session = await auth()
+
+  if(!session || !session.user){
+    return{
+      errors: {
+        _form: ['You must be logged in to create a topic']
+      }
+    }
+  }
+
+  return {
+    errors: {}
+  }
   // TODO:REVALIDATE THE HOMEPAGE
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import {
   Input,
   Button,
@@ -9,13 +12,16 @@ import {
 import * as actions from "@/app/actions";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
@@ -23,13 +29,22 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe Your Topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form.join(", ")}
+              </div>
+            ) : null}
             <Button type="submit">Submit</Button>
           </div>
         </form>
