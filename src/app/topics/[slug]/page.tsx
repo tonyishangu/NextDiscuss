5076e3807a0fe878a2233818805dfe7d1d777f8b
tonyishangu <tonyishangu@gmail.com>
2024-04-18 22:02:13 +0300

    scaffolding the topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,5 +1,18 @@
-export default function ShowTopic () {
+interface TopicShowProps {
+    params: {
+        slug: string
+    }
+}
+
+export default function ShowTopic ({params}: TopicShowProps) {
+    const { slug } = params
     return (
-        <div>Show Topics</div>
+        <div className='grid grid-cols-4 gap-4 p-4'>
+            <div className="col-span-3">
+                <h1 className="text-2xl font-bold mb-2">
+                    {slug}
+                </h1>
+            </div>
+        </div>
     )
 }
